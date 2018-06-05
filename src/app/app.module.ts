4b1b43782caa15bedc170e01d2a1cfe1fb80f389
Siriwasan <siriwasan@gmail.com>
2018-06-06 05:18:41 +0700

    add RadarChart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarChartComponent,
     LineChartComponent,
-    PieChartComponent
+    PieChartComponent,
+    RadarChartComponent
   ],
   imports: [
     BrowserModule
