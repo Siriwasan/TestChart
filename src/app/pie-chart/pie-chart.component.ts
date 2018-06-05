552e1d65fe1c0198d107c91c2d84be61b6f5000b
Siriwasan <siriwasan@gmail.com>
2018-06-06 05:13:01 +0700

    add LineChart component

---
+++ src/app/pie-chart/pie-chart.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.css']
+})
+export class PieChartComponent implements OnInit {
+
+  chart: object;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.chart = new Chart('pie-chart', {
+      type: 'pie',
+      data: {
+        labels: ['Africa', 'Asia', 'Europe', 'Latin America', 'North America'],
+        datasets: [{
+          label: 'Population (millions)',
+          backgroundColor: ['#3e95cd', '#8e5ea2','#3cba9f', '#e8c3b9', '#c45850'],
+          data: [2478, 5267, 734, 784, 433]
+        }]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Predicted world population (millions) in 2050'
+        }
+      }
+  });
+  }
+
+}
